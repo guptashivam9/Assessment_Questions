8a2678743c1d0ebd59575777b8c5cb43b3a4b83e
guptashivam9 <shivam98042@gmail.com>
2026-02-06 22:03:28 +0545

    Complete the question_no_2 ,where sliding window concepts are used to find minimum sub string.

--- Ques_no_2/main.js
+++ Ques_no_2/main.js
@@ -1,3 +1,41 @@
 function minSubString(s,t){
+    if(s.length < t.length){
+        return "";
+    }
+
+    let table = new Map();
+    for(let ch of t){
+        table.set(ch,(table.get(ch) ?? 0) + 1);
+    }
+
+    let i = 0 , j = 0 , req = t.length , minLength = Infinity , start = 0;
+    for( ; j < s.length; j++){
+        let rchar = s[j];
+        if(table.has(rchar)){
+            if(table.get(rchar)>0){
+                req--; 
+            }
+           
+            table.set(rchar,table.get(rchar)-1)
+        }
+        while(req === 0){
+            if(j-i+1 < minLength){
+                minLength = j-i+1;
+                start = i;
+            }
+            let lchar = s[i];
+            if(table.has(lchar)){
+                table.set(lchar,(table.get(lchar))+1);
+                if(table.get(lchar)>0){
+                    req++;
+                }
+            }
+            i++;
+        }
+    }
+    return minLength === Infinity ? "" : s.substring(start, start + minLength);
 
 }
+
+let result = minSubString("ADOBECODEBANC","ABC");
+console.log(result);
